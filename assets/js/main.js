9135916e049ca28bca67bb94457af392f1a9b414
José Mora <jmora000@outlook.com>
2022-06-29 00:14:45 -0300

    Desafio 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,68 @@
-// Ejemplo 1
- alert("A continuacion se pedirá que ingrese un numero. Al mismo se le sumará el numero 5, 10 veces.")
-let num = parseInt(prompt("Ingrese el numero:"))
-
-for (let i=1 ; i<=10 ; i++){
-    console.log(i + ": " + num + ".")
-    num += 5
+// Funcion que permite realizar una division no decimal.
+function intDivision (num, divisor){
+    const resto = num % divisor 
+    return (num - resto) / divisor
 }
-alert("Ejemplo 1 finalizado, los resultado se muestran en consola")
 
-//Ejemplo 2
-alert("Ahora, se pedira un texto en cada repeticion y se ira sumando al texto ingresado previamente. Ingresar \"esc\" finalizará el proceso")
-let texto = prompt("Ingrese el texto:")
-let textoTotal = ""
-while (texto.toLocaleLowerCase() != "esc"){
-    textoTotal += texto
-    console.log(textoTotal)
-    texto = prompt("Ingrese otro texto (\"esc\" para finalizar):")
+// Funcion que extrae la cantidad de horas de una cantidad de segundos pasada como parametro.
+function extractHours (seconds){
+    return intDivision(seconds, 3600)
 }
-alert("Ejemplo 2 finalizado, los resultado se muestran en consola")
 
-//Ejemplo 3
-alert("Por ultimo, ingrese un numero y se mostrará el texto \"Hola\" la cantidad de veces ingresada")
-let cant = parseInt(prompt("Ingrese un numero:"))
-let str = ""
-for (let i=0 ; i<cant ; i++){
-    str += "hola "
+// Funcion que extrae la cantidad de minutos de una cantidad de segundos pasada como parametro.
+function extractMinutes (seconds){
+    return intDivision((seconds%3600), 60)
 }
-console.log(str)
-alert("Ejemplo 3 finalizado, los resultado se muestran en consola")
-alert("Programa finalizado")
+
+// Funcion que extrae la cantidad de segundos (descontando minutos y horas) 
+// de una cantidad de segundos pasada como parametro.
+function extractSeconds (seconds){
+    return (seconds%3600)%60
+}
+
+// Funcion que muestra en formato reloj un tiempo determinado en segundos.
+function printTime (seconds){
+    let hours = extractHours(seconds)
+    if (hours < 10) {hours = "0" + hours} //Para darle formato de hora "01, 02, ..., 09"
+    let min = extractMinutes(seconds)
+    if (min < 10) {min = "0" + min}
+    let sec = extractSeconds(seconds)
+    if (sec < 10) {sec = "0" + sec}
+    return hours + ":" + min + ":" + sec
+}
+
+// Inicio del codigo.
+alert("Configure su reloj para utilizar el método de estudio pomodoro!")
+// Variables de configuracion.
+const studySeconds = (parseInt(prompt("Ingrese la cantidad de MINUTOS que desea estudiar"))) * 60
+const breakSeconds = (parseInt(prompt("Ingrese la cantidad de MINUTOS que desea descansar entre cada pomodoro"))) * 60
+const pomodoros = parseInt(prompt("Ingrese la cantidad de pomodoros que desea estudiar (Se recomienda entre 2 y 5)"))
+const totalStudyTime = pomodoros * studySeconds
+
+// Variables de conteo
+let actualPomo = 0 //Este es el pomodoro actual 
+let actualTime = 0
+// Mientras no se completen los pomodoros indicados
+while (actualPomo <= pomodoros){
+    alert("Comienza el pomodoro numero " + actualPomo + "!")
+    // Se setea el tiempo de estudio y comienza el mismo.
+    actualTime = studySeconds
+    while (actualTime != 0){
+        console.log("Estudio: " + printTime(actualTime)) // Se muestra el tiempo
+        actualTime-- //Pasa un segundo
+    }
+    // Se comprueba que no sea la ultima vuelta (ultimo pomodoro).
+    if (actualPomo != pomodoros){
+        alert("Pomodoro " + actualPomo + " finalizado, comienza el break.")
+        // Se setea el tiempo de break y comienza el mismo.
+        actualTime = breakSeconds
+        while (actualTime != 0){
+            console.log("Break: " + printTime(actualTime)) // Se muestra el tiempo
+            actualTime-- //Pasa un segundo
+        }
+    }else {
+        alert("Finalizo el estudio!")
+        alert("Tiempo total estudiado: " + printTime(totalStudyTime))
+    }
+    actualPomo++
+}
